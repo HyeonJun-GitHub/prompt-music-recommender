4089e29751be63d4a20b8902f89b6d30b9a2915f
architecture_hyeonjun <kim.hyeonjun@kt.com>
2024-09-10 23:52:55 +0900

    test

--- rag_web.py
+++ rag_web.py
@@ -97,7 +97,7 @@ my_slider = st.slider(
 selected_date = int_to_date(my_slider)
 
 # 선택된 날짜 출력
-st.write(f"검색 기간 : {selected_date.strftime('%Y년 %m월 %d일')} ~ {current_date.strftime('%Y년 %m월 %d일')}")
+st.write(f"검색 기간 : {selected_date.strftime('%Y년 %m월 %d일')} ~ 현재 날짜")
 
 # 검색 함수들
 def search_by_artist_id(artist_ids_prompt):
