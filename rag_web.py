3bcee1fc1157123da1178e1d693329f64ac67738
architecture_hyeonjun <kim.hyeonjun@kt.com>
2024-09-10 23:40:16 +0900

    ㅅㄷㄴㅅ

--- rag_web.py
+++ rag_web.py
@@ -199,7 +199,9 @@ with st.expander("프롬프트 입력", expanded=True):
         prompt = st.text_area("무슨 노래가 듣고 싶어요?")
     with col2:
         st.write("")  # 빈 줄 추가
-        search_button_clicked = st.button("프롬프트 검색")
+        col1, col2 = st.columns([4, 1])  # 비율을 4:1로 설정하여 왼쪽에 입력, 오른쪽에 버튼 배치
+        with col1:
+            search_button_clicked = st.button("프롬프트 검색")
     
     if search_button_clicked:
         search(prompt)
