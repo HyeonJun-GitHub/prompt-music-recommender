80ea41925b79114a0aef92d26bd8eccdb2318880
architecture_hyeonjun <kim.hyeonjun@kt.com>
2024-09-11 10:26:02 +0900

    ㅅㅈ

--- rag_web.py
+++ rag_web.py
@@ -5,35 +5,8 @@ from datetime import datetime, timedelta
 import os
 import base64
 from PIL import Image
-import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
-import streamlit as st
-import streamlit.components.v1 as components
-
-# JavaScript를 사용하여 userAgent를 가져오는 코드
-user_agent = components.html("""
-    <script>
-        const userAgent = navigator.userAgent;
-        document.body.innerHTML = `<div id='userAgent'>${userAgent}</div>`;
-    </script>
-    <div id='userAgent'></div>
-    """, height=35)
-
-# user_agent 값을 사용하여 분기 처리
-if "Mobile" in user_agent:
-    spacer_height = """
-        <div style="height: 0px;"></div>
-    """
-    st.write("모바일 기기입니다.")
-else:
-    spacer_height = """
-        <div style="height: 28px;"></div>
-    """
-    st.write("PC입니다.")
-
-# HTML을 사용하여 spacer_height 값을 반영
-components.html(spacer_height, height=0)
 
 # JavaScript를 사용하여 userAgent를 가져오는 코드
 js_code = """
@@ -81,25 +54,25 @@ else:
         <div style="height: 28px;"></div>
     """
 
-# # HTML을 사용하여 spacer_height 값을 반영
-# components.html(spacer_height, height=0)
+# HTML을 사용하여 spacer_height 값을 반영
+components.html(spacer_height, height=0)
 
-# # User-Agent를 가져오는 함수
-# user_agent = st.experimental_get_query_params().get('user-agent', [''])[0]
-# if not user_agent:
-#     user_agent = st.session_state.get('user_agent', '')
-# if user_agent == '':
-#     st.warning("User-Agent 정보를 가져오는 중입니다...")
-# else:
-#     if "Mobile" in user_agent:
-#         spacer_height = """
-#             <div style="height: 0px;"></div>
-#         """
-#     else:
-#         spacer_height = """
-#             <div style="height: 28px;"></div>
-#         """
-#     st.session_state.user_agent = user_agent
+# User-Agent를 가져오는 함수
+user_agent = st.experimental_get_query_params().get('user-agent', [''])[0]
+if not user_agent:
+    user_agent = st.session_state.get('user_agent', '')
+if user_agent == '':
+    st.warning("User-Agent 정보를 가져오는 중입니다...")
+else:
+    if "Mobile" in user_agent:
+        spacer_height = """
+            <div style="height: 0px;"></div>
+        """
+    else:
+        spacer_height = """
+            <div style="height: 28px;"></div>
+        """
+    st.session_state.user_agent = user_agent
 
 # 로컬 이미지 경로 설정
 box_img_path = os.path.join(os.getcwd(), "box_01.png")
