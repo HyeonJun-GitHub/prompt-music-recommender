860b7e1d24ef55f83c2d9aa6765d6b571a9aa8e8
architecture_hyeonjun <kim.hyeonjun@kt.com>
2024-09-12 00:06:26 +0900

    ㅅㄷㄴㅅ

--- rag_web.py
+++ rag_web.py
@@ -313,7 +313,7 @@ def display_sample_results(data_info):
         # st.markdown(f"[{song_name} - {artist_name}](https://genie.co.kr/detail/songInfo?xgnm={song_id})")
         st.markdown(f"""
             <div style='text-align: center; background-color: black; padding: 5px 0; border-radius: 2px;'>
-                <a href='https://genie.co.kr/detail/songInfo?xgnm={song_id}' style='color: white; text-decoration: none; font-size: 20px; padding: 10px 20px; display: inline-block; background-color: #333; border-radius: 10px;'> 
+                <a href='https://genie.co.kr/detail/songInfo?xgnm={song_id}' style='color: white; text-decoration: none; font-size: 15px; padding: 10px 20px; display: inline-block; background-color: #333; border-radius: 2px;'> 
                     {song_name} - {artist_name} 
                 </a>
             </div>
