4db5785f2d60d5f814b782e9d3074c6618113de3
architecture_hyeonjun <kim.hyeonjun@kt.com>
2024-09-11 13:57:49 +0900

    ㅅㄷㅌㅅ

--- rag_web.py
+++ rag_web.py
@@ -89,9 +89,8 @@ st.markdown(text_area_style, unsafe_allow_html=True)  # 텍스트 입력창 스�
 
 st.image(title_00_img, caption='', use_column_width=True)
 # 레이아웃 시작
-st.write("")
 st.title("국가 선택")
-st.subheader("국가 선택")
+st.write("국가 선택")
 segment = st.radio("", ("전체", "국내", "해외"))
 
 album_release_country = ""
