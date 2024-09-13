be9c51ff4cad566ad2dbc01862bdcfad8f7d4a56
HyeonJun-GitHub <61862658+HyeonJun-GitHub@users.noreply.github.com>
2024-09-13 20:18:02 +0900

    test

--- rag_web.py
+++ rag_web.py
@@ -364,7 +364,7 @@ def search_api(query, mode="songs"):
                 "name": item["songs_name"].get("original", "Unknown Artist"),
                 "id": item["songs_id"]
             }
-            for item in data.get('searchResult', {}).get('result', {}).get(mode, {}).get('items', [])
+            for item in data.get('searchResult', {}).get('result', {}).get("songs", {}).get('items', [])
         ]
         
         # 이름과 ID 리스트로 분리하여 반환
