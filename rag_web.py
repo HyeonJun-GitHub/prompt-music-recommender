d471e387496770d1f73e45bed757531abe104ebb
HyeonJun-GitHub <61862658+HyeonJun-GitHub@users.noreply.github.com>
2024-09-13 20:48:35 +0900

    test

--- rag_web.py
+++ rag_web.py
@@ -424,9 +424,9 @@ with st.expander("유사 곡 검색"):
     # 텍스트 입력창과 버튼을 같은 너비로 하기 위해 컨테이너 사용
     with st.container():
         if selected_song_name and selected_song_id:
-            song_search_button_clicked = st.button(f"{selected_song_name} : 곡 검색", use_container_width=True)
+            song_search_button_clicked = st.button(f"{selected_song_name} : 유사 검색", use_container_width=True)
         else:
-            song_search_button_clicked = st.button("곡 검색", use_container_width=True)
+            song_search_button_clicked = st.button("곡 통합 검색", use_container_width=True)
     
     if song_search_button_clicked and selected_song_id:
         with st.spinner('AI가 플레이리스트를 만드는 중입니다...'):
@@ -459,7 +459,7 @@ with st.expander("유사 아티스트 검색"):
         if selected_artist_name and selected_artist_id:
             artist_search_button_clicked = st.button(f"{selected_artist_name} : 아티스트 검색", use_container_width=True)
         else:
-            artist_search_button_clicked = st.button("아티스트 검색", use_container_width=True)
+            artist_search_button_clicked = st.button("아티스트 통합 검색", use_container_width=True)
     
     if artist_search_button_clicked and selected_artist_id:
         with st.spinner('AI가 플레이리스트를 만드는 중입니다...'):
