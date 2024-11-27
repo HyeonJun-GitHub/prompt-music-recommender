2af00e7cba04ddd6918cec281d34cc466d7a21be
architecture_hyeonjun <kim.hyeonjun@kt.com>
2024-11-27 20:36:59 +0900

    test

--- pages/AI_Agent.py
+++ pages/AI_Agent.py
@@ -121,120 +121,122 @@ st.markdown(text_area_style, unsafe_allow_html=True)  # 텍스트 입력창 스�
 
 # -------------------------------------------------------------
 
-# with st.sidebar:
-#     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+# import streamlit as st
+# from streamlit_chat import message
+# from streamlit.components.v1 import html
 
-# st.title("💬 AI Curation Chatbot")
-# if "messages" not in st.session_state:
-#     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+# def on_input_change():
+#     user_input = st.session_state.user_input
+#     st.session_state.past.append(user_input)
+#     st.session_state.generated.append("The messages from Bot\nWith new line")
 
-# if st.button("대화 삭제"):
-#     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+# def on_btn_click():
+#     del st.session_state.past[:]
+#     del st.session_state.generated[:]
 
-# for msg in st.session_state.messages:
-#     st.chat_message(msg["role"]).write(msg["content"])
+# audio_path = "https://docs.google.com/uc?export=open&id=16QSvoLWNxeqco_Wb2JvzaReSAw5ow6Cl"
+# img_path = "https://www.groundzeroweb.com/wp-content/uploads/2017/05/Funny-Cat-Memes-11.jpg"
+# youtube_embed = '''
+# <iframe width="400" height="215" src="https://www.youtube.com/embed/LMQ5Gauy17k" title="YouTube video player" frameborder="0" allow="accelerometer; encrypted-media;"></iframe>
+# '''
 
-# if prompt := st.chat_input():
+# markdown = """
+# ### HTML in markdown is ~quite~ **unsafe**
+# <blockquote>
+#   However, if you are in a trusted environment (you trust the markdown). You can use allow_html props to enable support for html.
+# </blockquote>
 
-#     st.session_state.messages.append({"role": "user", "content": prompt})
-#     st.chat_message("user").write(prompt)
-#     msg = "Hi (Debug)" 
-#     st.session_state.messages.append({"role": "assistant", "content": msg})
-#     st.chat_message("assistant").write(msg)
+# * Lists
+# * [ ] todo
+# * [x] done
 
-import streamlit as st
-from streamlit_chat import message
-from streamlit.components.v1 import html
+# Math:
 
-def on_input_change():
-    user_input = st.session_state.user_input
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append("The messages from Bot\nWith new line")
+# Lift($L$) can be determined by Lift Coefficient ($C_L$) like the following
+# equation.
 
-def on_btn_click():
-    del st.session_state.past[:]
-    del st.session_state.generated[:]
+# $$
+# L = \\frac{1}{2} \\rho v^2 S C_L
+# $$
 
-audio_path = "https://docs.google.com/uc?export=open&id=16QSvoLWNxeqco_Wb2JvzaReSAw5ow6Cl"
-img_path = "https://www.groundzeroweb.com/wp-content/uploads/2017/05/Funny-Cat-Memes-11.jpg"
-youtube_embed = '''
-<iframe width="400" height="215" src="https://www.youtube.com/embed/LMQ5Gauy17k" title="YouTube video player" frameborder="0" allow="accelerometer; encrypted-media;"></iframe>
-'''
+# ~~~py
+# import streamlit as st
 
-markdown = """
-### HTML in markdown is ~quite~ **unsafe**
-<blockquote>
-  However, if you are in a trusted environment (you trust the markdown). You can use allow_html props to enable support for html.
-</blockquote>
+# st.write("Python code block")
+# ~~~
 
-* Lists
-* [ ] todo
-* [x] done
+# ~~~js
+# console.log("Here is some JavaScript code")
+# ~~~
 
-Math:
+# """
 
-Lift($L$) can be determined by Lift Coefficient ($C_L$) like the following
-equation.
+# table_markdown = '''
+# A Table:
 
-$$
-L = \\frac{1}{2} \\rho v^2 S C_L
-$$
+# | Feature     | Support              |
+# | ----------: | :------------------- |
+# | CommonMark  | 100%                 |
+# | GFM         | 100% w/ `remark-gfm` |
+# '''
 
-~~~py
-import streamlit as st
+# st.session_state.setdefault(
+#     'past', 
+#     ['plan text with line break',
+#      'play the song "Dancing Vegetables"', 
+#      'show me image of cat', 
+#      'and video of it',
+#      'show me some markdown sample',
+#      'table in markdown']
+# )
+# st.session_state.setdefault(
+#     'generated', 
+#     [{'type': 'normal', 'data': 'Line 1 \n Line 2 \n Line 3'},
+#      {'type': 'normal', 'data': f'<audio controls src="{audio_path}"></audio>'}, 
+#      {'type': 'normal', 'data': f'<img width="100%" height="200" src="{img_path}"/>'}, 
+#      {'type': 'normal', 'data': f'{youtube_embed}'},
+#      {'type': 'normal', 'data': f'{markdown}'},
+#      {'type': 'table', 'data': f'{table_markdown}'}]
+# )
 
-st.write("Python code block")
-~~~
+# st.title("Chat placeholder")
 
-~~~js
-console.log("Here is some JavaScript code")
-~~~
+# chat_placeholder = st.empty()
 
-"""
+# with chat_placeholder.container():    
+#     for i in range(len(st.session_state['generated'])):                
+#         message(st.session_state['past'][i], is_user=True, key=f"{i}_user")
+#         message(
+#             st.session_state['generated'][i]['data'], 
+#             key=f"{i}", 
+#             allow_html=True,
+#             is_table=True if st.session_state['generated'][i]['type']=='table' else False
+#         )
 
-table_markdown = '''
-A Table:
+#     st.button("Clear message", on_click=on_btn_click)
 
-| Feature     | Support              |
-| ----------: | :------------------- |
-| CommonMark  | 100%                 |
-| GFM         | 100% w/ `remark-gfm` |
-'''
+# with st.container():
+#     st.text_input("User Input:", on_change=on_input_change, key="user_input")
 
-st.session_state.setdefault(
-    'past', 
-    ['plan text with line break',
-     'play the song "Dancing Vegetables"', 
-     'show me image of cat', 
-     'and video of it',
-     'show me some markdown sample',
-     'table in markdown']
-)
-st.session_state.setdefault(
-    'generated', 
-    [{'type': 'normal', 'data': 'Line 1 \n Line 2 \n Line 3'},
-     {'type': 'normal', 'data': f'<audio controls src="{audio_path}"></audio>'}, 
-     {'type': 'normal', 'data': f'<img width="100%" height="200" src="{img_path}"/>'}, 
-     {'type': 'normal', 'data': f'{youtube_embed}'},
-     {'type': 'normal', 'data': f'{markdown}'},
-     {'type': 'table', 'data': f'{table_markdown}'}]
-)
+# -------------------------------------------------------------
 
-st.title("Chat placeholder")
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 
-chat_placeholder = st.empty()
+st.title("💬 AI Curation Chatbot")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-with chat_placeholder.container():    
-    for i in range(len(st.session_state['generated'])):                
-        message(st.session_state['past'][i], is_user=True, key=f"{i}_user")
-        message(
-            st.session_state['generated'][i]['data'], 
-            key=f"{i}", 
-            allow_html=True,
-            is_table=True if st.session_state['generated'][i]['type']=='table' else False
-        )
+if st.button("대화 삭제"):
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-    st.button("Clear message", on_click=on_btn_click)
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-with st.container():
-    st.text_input("User Input:", on_change=on_input_change, key="user_input")
+if prompt := st.chat_input():
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    msg = "Hi (Debug)" 
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
