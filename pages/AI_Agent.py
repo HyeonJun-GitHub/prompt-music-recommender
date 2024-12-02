d821f3607c90bc8284995dbe26425c5a2d0d23ac
architecture_hyeonjun <kim.hyeonjun@kt.com>
2024-12-02 16:12:48 +0900

    ㅅㄷㄴㅅ

--- pages/AI_Agent.py
+++ pages/AI_Agent.py
@@ -606,7 +606,7 @@ def on_input_change():
         client = OpenAI(api_key=openai_api_key)
         response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
         msg = response.choices[0].message.content
-        # st.session_state.messages.append({"role": "assistant", "content": msg})
+        st.session_state.messages.append({"role": "assistant", "content": msg})
         # st.chat_message("assistant").write(msg)
         st.session_state.generated.append(msg)
 
