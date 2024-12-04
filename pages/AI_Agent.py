30ddb9bd177228b2e60d6567b1c3608f4a48b96f
architecture_hyeonjun <kim.hyeonjun@kt.com>
2024-12-04 13:52:38 +0900

    test

--- pages/AI_Agent.py
+++ pages/AI_Agent.py
@@ -189,31 +189,7 @@ At the end of the loop you output an Answer.
 Use Thought to describe your thoughts about the question you have been asked.
 Use Action to run one of the actions available to you - then return PAUSE.
 Observation will be the result of running those actions.
-                  
-calculate:
-e.g. calculate: 4 * 7 / 3
-Runs a calculation and returns the number - uses Python so be sure to use floating point
-syntax if necessary
 
-wikipedia:
-e.g. wikipedia: Oasis
-Returns a summary from searching Wikipedia
-
-search_api:
-e.g. search_api: Oasis
-Search Simon's blog for that term
-
-Example session:
-Question: What is the capital of France?
-Thought: I should look up France on Wikipedia
-Action: wikipedia: France
-PAUSE
-
-You will be called again with this:
-Observation: France is a country. The capital is Paris.
-
-You then output:
-Answer: The capital of France is Paris
 """.strip()
 # chat_bot = ChatBot(system=bot_prompt)
 
